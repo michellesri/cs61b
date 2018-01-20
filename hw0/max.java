662172beda716ea8f1502a42ab730505effb421f
Michelle Sri <michelle.sri@gmail.com>
2018-01-20 15:56:27 -0800

    added max.java

---
+++ hw0/max.java
@@ -0,0 +1,15 @@
+public class maxOfArray {
+   public static int max(int[] m) {
+    int max = m[0];
+     for (int count = 1; count < m.length; count++) {
+         if (m[count] > max) {
+          max = m[count];
+        }
+    }
+    return max;
+   }
+   public static void main(String[] args) {
+      int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
+      System.out.print(max(numbers));
+   }
+}
