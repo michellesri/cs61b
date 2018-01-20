ebffb6024f195251710cc878ce29c15d80f0d42c
Michelle Sri <michelle.sri@gmail.com>
2018-01-20 15:37:18 -0800

    added 1b triangleDemo

---
+++ hw0/1b.java
@@ -0,0 +1,15 @@
+public class triangleDemo {
+    public static void main(String[] args) {
+      drawTriangle(10);
+    }
+    public static String drawTriangle(int N) {
+      String triangle = "";
+      int counter = 0;
+      while (counter < N) {
+        triangle += "*";
+        counter += 1;
+        System.out.println(triangle);
+      }
+      return triangle;
+    }
+}
