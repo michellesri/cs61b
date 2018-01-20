7ead1038a2ff4f66d370b5ac862eb092c97701fe
Michelle Sri <michelle.sri@gmail.com>
2018-01-20 13:32:12 -0800

    added javaNotes

---
+++ javaNotes/simplePrint.java
@@ -0,0 +1,13 @@
+public class ClassNameHere {
+   public static void main(String[] args) {
+      int x = 5;
+      
+      if (x < 10) 
+         x = x + 10;
+      
+      if (x < 10) 
+         x = x + 10;
+      
+      System.out.println(x);
+   }
+}
