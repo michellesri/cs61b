dde1bbc8a5337d90ed722ec2d2a295cf1a424fa1
Michelle Sri <michelle.sri@gmail.com>
2018-01-20 13:42:33 -0800

    hw0 print hashes

---
+++ hw0/1a.java
@@ -0,0 +1,17 @@
+public class myHashes {
+  public static void main(String[] args) {
+    String myString = "";
+    int a = 0;
+    while (a <= 5) {
+      myString += "#";
+      System.out.println(myString);
+      a += 1;
+    }
+  }
+}
+
+// *
+// **
+// ***
+// ****
+// *****
