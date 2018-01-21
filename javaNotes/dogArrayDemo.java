2ffbf6494e82f3e23aa742c4f0b5a1ad6717fc68
Michelle Sri <michelle.sri@gmail.com>
2018-01-21 12:30:49 -0800

    added dogArrayDemo

---
+++ javaNotes/dogArrayDemo.java
@@ -0,0 +1,11 @@
+public class DogArrayDemo {
+    public static void main(String[] args) {
+        /* Create an array of two dogs. */
+        Dog[] dogs = new Dog[2];
+        dogs[0] = new Dog(8); /*create an actual dog */
+        dogs[1] = new Dog(20);
+
+        /* Yipping will result, since dogs[0] has weight 8. */
+        dogs[0].makeNoise();
+    }
+}
