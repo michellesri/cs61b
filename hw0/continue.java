69130afc556f04f123107dc117ec2a87e5bf5b1d
Michelle Sri <michelle.sri@gmail.com>
2018-01-20 16:09:59 -0800

    added continue.java demo

---
+++ hw0/continue.java
@@ -0,0 +1,14 @@
+public class ContinueDemo {
+    public static void main(String[] args) {
+        String[] a = {"cat", "dog", "laser horse", "ketchup", "horse", "horbse"};
+
+        for (int i = 0; i < a.length; i += 1) {
+            if (a[i].contains("horse")) {
+                continue;
+            }
+            for (int j = 0; j < 3; j += 1) {
+                System.out.println(a[i]);
+            }
+        }
+    }
+}
