0af691aae699176c2d36ac7df083173e2513b0be
Michelle Sri <michelle.sri@gmail.com>
2018-01-21 12:50:52 -0800

    added staticMaxDog example

---
+++ javaNotes/staticMaxDog.java
@@ -0,0 +1,10 @@
+public static Dog maxDog(Dog d1, Dog d2) {
+    if (d1.weightInPounds > d2.weightInPounds) {
+        return d1;
+    }
+    return d2;
+}
+
+Dog d = new Dog(15);
+Dog d2 = new Dog(100);
+Dog.maxDog(d, d2);
