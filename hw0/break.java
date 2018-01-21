563b77590d6dcc9655ab1cc98bccb2e9ca81f10d
Michelle Sri <michelle.sri@gmail.com>
2018-01-20 16:09:44 -0800

    added break.java demo

---
+++ hw0/break.java
@@ -0,0 +1,14 @@
+public class BreakDemo {
+    public static void main(String[] args) {
+        String[] a = {"cat", "dog", "laser horse", "ketchup", "horse", "horbse"};
+
+        for (int i = 0; i < a.length; i += 1) {
+            for (int j = 0; j < 3; j += 1) {
+                System.out.println(a[i]);
+                if (a[i].contains("horse")) {
+                    break;
+                }                
+            }
+        }
+    }
+}
