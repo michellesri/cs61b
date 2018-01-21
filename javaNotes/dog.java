8496ddcfcd3ec2aedd07782bb1c6015fc32553bd
Michelle Sri <michelle.sri@gmail.com>
2018-01-21 12:17:12 -0800

    created dog class

---
+++ javaNotes/dog.java
@@ -0,0 +1,17 @@
+public class Dog {
+    public int weightInPounds;
+
+    public Dog(int w) {
+        weightInPounds = w;
+    }
+
+    public void makeNoise() {
+        if (weightInPounds < 10) {
+            System.out.println("yipyipyip!");
+        } else if (weightInPounds < 30) {
+            System.out.println("bark. bark.");
+        } else {
+            System.out.println("woof!");
+        }    
+    }
+}
