e3aaa80873e0ebae9f6358f103d8898790a96f84
Michelle Sri <michelle.sri@gmail.com>
2018-01-21 14:35:51 -0800

    fixed errors in fib2.java

--- disc01/fib2.java
+++ disc01/fib2.java
@@ -1,9 +1,9 @@
-public static int fib2(int n, int k, int f0, int f1) {
+public class fib2 {
   public static void main(String[] args) {
     System.out.println(fibonacci2(3));
   }
   
-  public static int fibonacci2(int n) {
+  public static int fib2(int n, int k, int f0, int f1) {
     if (n == k) {
       return f0;
     } else {
