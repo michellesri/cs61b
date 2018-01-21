d9efd9cb0678da8074b9e9ca728dfa350d47c17c
Michelle Sri <michelle.sri@gmail.com>
2018-01-21 12:51:42 -0800

    added instanceMaxDog demo

---
+++ javaNotes/instanceMaxDog.java
@@ -0,0 +1,10 @@
+public Dog maxDog(Dog d2) {
+    if (this.weightInPounds > d2.weightInPounds) {
+        return this;
+    }
+    return d2;
+}
+
+Dog d = new Dog(15);
+Dog d2 = new Dog(100);
+d.maxDog(d, d2);
