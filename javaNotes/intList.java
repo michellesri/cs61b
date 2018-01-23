3f49ec58de3883d24dfd09ff37f74b2401ea6c29
Michelle Sri <michelle.sri@gmail.com>
2018-01-23 15:17:49 -0800

    created iterative method for finding size of an intlist

--- javaNotes/intList.java
+++ javaNotes/intList.java
@@ -11,6 +11,8 @@ public class IntList {
         IntList L = new IntList(15, null);
         L = new IntList(10, L);
         L = new IntList(5, L);
+
+        System.out.println(L.size())
     }
 
     public int size() {
@@ -19,5 +21,17 @@ public class IntList {
             return 1;
         }
         return 1 + this.rest.size();
+    }
+
+    public int interativeSize() {
+        // return the size of the list iteratively
+        IntList p = this;
+        int totalSize = 0;
+
+        while (p != null) {
+            totalSize += 1;
+            p = p.rest;
+        }
+        return totalSize;
     }
 }
