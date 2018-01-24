78bf17c1c766096ab054417c3bc0118028b0df35
Michelle Sri <michelle.sri@gmail.com>
2018-01-24 08:48:09 -0800

    finished readPlanets method

--- proj0/NBody.java
+++ proj0/NBody.java
@@ -1,3 +1,5 @@
+//import java.util.Arrays;
+
 public class NBody {
     public static double readRadius(String fileName) {
         In in = new In(fileName);
@@ -6,7 +8,32 @@ public class NBody {
         double radiusUniverse = in.readDouble();
 
         return radiusUniverse;
+    }
+
+    public static Planet[] readPlanets(String fileName) {
+        In in = new In(fileName);
+
+        int numPlanets = in.readInt();
+        double radiusUniverse = in.readDouble();
+
+        Planet[] planetsInFile = new Planet[numPlanets];
 
 
+        int counter = 0;
+        while (counter < numPlanets) {
+            double xxPos = in.readDouble();
+            double yyPos = in.readDouble();
+
+            double xxVel = in.readDouble();
+            double yyVel = in.readDouble();
+            double mass = in.readDouble();
+            String imgFileName = in.readString();
+
+            planetsInFile[counter] = new Planet(xxPos, yyPos, xxVel,
+            yyVel, mass, imgFileName);
+            counter++;
+
+        }
+        return planetsInFile;
     }
 }
