0ec42a0e3e0b5e8d46f2528fb83510a3c09c31a4
Michelle Sri <michelle.sri@gmail.com>
2018-01-24 07:06:51 -0800

    finished writing planet constructors and calcDistance method

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,18 +1,35 @@
 public class Planet {
-    public double xxPos = 1.0;
-    public double yyPos = 2.0;
-    public double xxVel = 3.0;
-    public double yyVel = 4.0;
-    public double mass = 5.0;
-    public String imgFileName = "jupiter.gif";
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
 
 
     public Planet(double xP, double yP, double xV,
                   double yV, double m, String img) {
-
+        this.xxPos = xP;
+        this.yyPos = yP;
+        this.xxVel = xV;
+        this.yyVel = yV;
+        this.mass = m;
+        this.imgFileName = img;
     }
 
     public Planet(Planet p) {
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+    }
+
+    public double calcDistance(Planet p1) {
+        double dx = this.xxPos - p1.xxPos;
+        double dy = this.yyPos - p1.yyPos;
+        return Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
     }
 
 }
