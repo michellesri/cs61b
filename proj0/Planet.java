e2a98986a825816897adbdd0c5d88184c3ea6430
Michelle Sri <michelle.sri@gmail.com>
2018-01-23 17:53:01 -0800

    created planet class with 2 constructors. all tests passing

---
+++ proj0/Planet.java
@@ -0,0 +1,18 @@
+public class Planet {
+    public double xxPos = 1.0;
+    public double yyPos = 2.0;
+    public double xxVel = 3.0;
+    public double yyVel = 4.0;
+    public double mass = 5.0;
+    public String imgFileName = "jupiter.gif";
+
+
+    public Planet(double xP, double yP, double xV,
+                  double yV, double m, String img) {
+
+    }
+
+    public Planet(Planet p) {
+    }
+
+}
