df0e5639bfa1836c7110436eb2b56b4c3553e107
Michelle Sri <michelle.sri@gmail.com>
2018-01-25 18:16:50 -0800

    finished writing sllist methods

--- javaNotes/sllist.java
+++ javaNotes/sllist.java
@@ -7,8 +7,24 @@ public class SLList {
     first = new IntNode(x, null);
   }
 
+
+  public void addFirst(int x) {
+    // adds x to the front of the list
+    first = new IntNode(x, first);
+  }
+
+  public int getFirst() {
+    // returns the first item in the list
+    return first.item;
+  }
+
   public static void main(String[] args) {
     // creates a list of one integer, 10.
-    SLList L = new SLList(10);
+    SLList L = new SLList(15);
+    L.addFirst(10);
+    L.addFirst(5);
+
+    System.out.println(L.getFirst()); //should return 5
   }
+
 }
