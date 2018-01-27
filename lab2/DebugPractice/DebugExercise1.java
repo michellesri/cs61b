d832b5210f656aa3f1d69894de764d9e541909e2
Michelle Sri <michelle.sri@gmail.com>
2018-01-26 17:14:26 -0800

    finished debugExercise 1 and 2

--- lab2/DebugPractice/DebugExercise1.java
+++ lab2/DebugPractice/DebugExercise1.java
@@ -4,8 +4,8 @@
  */
 public class DebugExercise1 {
     public static int divideThenRound(int top, int bottom) {
-        int quotient = top / bottom;
-        int result = Math.round(quotient);
+        double quotient = (top * 1.0) / bottom;
+        int result = (int)Math.round(quotient);
         return result;
     }
 
