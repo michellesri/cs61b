e8cf0a9e2f5b5502d897988134691e984db6f4ce
Michelle Sri <michelle.sri@gmail.com>
2018-01-28 14:24:50 -0800

    notes for lecture 6 with starter code

---
+++ javaNotes/src/java/AList.java
@@ -0,0 +1,40 @@
+/** Array based list.
+ *  @author Josh Hug
+ */
+
+
+//
+public class AList {
+    int[] items;
+    int size;
+
+    /** Creates an empty list. */
+    public AList() {
+        items = new int[100]; //arbitrary size
+        size = 0;
+    }
+
+    /** Inserts X into the back of the list. */
+    public void addLast(int x) {
+    }
+
+    /** Returns the item from the back of the list. */
+    public int getLast() {
+        return 0;        
+    }
+    /** Gets the ith item in the list (0 is the front). */
+    public int get(int i) {
+        return 0;        
+    }
+
+    /** Returns the number of items in the list. */
+    public int size() {
+        return 0;        
+    }
+
+    /** Deletes item from back of the list and
+      * returns deleted item. */
+    public int removeLast() {
+        return 0;
+    }
+} 
