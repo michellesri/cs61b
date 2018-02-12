950c413ec6cc3d80d9da49fbad11c1668b615b5e
Michelle Sri <michelle.sri@gmail.com>
2018-02-11 19:43:41 -0800

    copied arraydeque and linkedlistdeque over from proj1a

---
+++ proj1b/ArrayDeque.java
@@ -0,0 +1,150 @@
+
+public class ArrayDeque<T> {
+    // placeholderType allows us to not immediately define type as int or str.
+    private int size;
+    private int firstIndex;
+    private int lastIndex;
+    private T[] myArrayDeque;
+
+    private static final int START_SIZE = 8;
+
+    public ArrayDeque() { // constructor for ArrayDeque
+        myArrayDeque = (T[]) new Object[START_SIZE];
+        firstIndex = 0;
+        lastIndex = 0;
+    }
+
+    public void addFirst(T x) {
+        if (size == 0) {
+            firstIndex = 0;
+            lastIndex = 0;
+            myArrayDeque[0] = x;
+            size++;
+            return;
+        }
+        if (size == myArrayDeque.length) {
+            resizeUp(); // resize my array here
+        }
+        if (firstIndex == 0) {
+            firstIndex = myArrayDeque.length - 1;
+        } else {
+            firstIndex--;
+        }
+        myArrayDeque[firstIndex] = x;
+        size++;
+    }
+
+    public void addLast(T x) {
+        if (size == 0) {
+            firstIndex = 0;
+            lastIndex = 0;
+            myArrayDeque[0] = x;
+            size++;
+            return;
+        }
+        if (size == myArrayDeque.length) {
+            resizeUp(); // resize my array here
+        }
+        if (lastIndex == myArrayDeque.length - 1) {
+            lastIndex = 0;
+        } else {
+            lastIndex++;
+        }
+        myArrayDeque[lastIndex] = x;
+        size++;
+    }
+
+    public T get(int index) {
+        return myArrayDeque[(firstIndex + index) % myArrayDeque.length];
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        if (size <= 0) {
+            return 0;
+        }
+        return size;
+    }
+
+    public void printDeque() {
+        for (int i = 0; i < myArrayDeque.length; i++) {
+            System.out.print(myArrayDeque[i] + " ");
+        }
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        T removedNode = myArrayDeque[firstIndex];
+        myArrayDeque[firstIndex] = null;
+        if (firstIndex == myArrayDeque.length - 1) {
+            firstIndex = 0;
+        } else {
+            firstIndex++;
+        }
+        size--;
+        if (size == 0) {
+            firstIndex = 0;
+            lastIndex = 0;
+        }
+        if (size < myArrayDeque.length / 4) {
+            resizeDown();
+        }
+        return removedNode;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        T removedNode = myArrayDeque[lastIndex];
+        myArrayDeque[lastIndex] = null;
+        if (lastIndex == 0) {
+            lastIndex = myArrayDeque.length - 1;
+        } else {
+            lastIndex--;
+        }
+        size--;
+        if (size == 0) {
+            firstIndex = 0;
+            lastIndex = 0;
+        }
+        if (size < myArrayDeque.length / 4) {
+            resizeDown();
+        }
+        return removedNode;
+    }
+
+    private void resizeUp() {
+        T[] resizedArray = (T[]) new Object[myArrayDeque.length * 2];
+        // copy array into new array
+        int sizeOfFirstCopy = myArrayDeque.length - firstIndex;
+        System.arraycopy(myArrayDeque, firstIndex, resizedArray, 0, sizeOfFirstCopy);
+        System.arraycopy(myArrayDeque, 0, resizedArray, sizeOfFirstCopy, size - sizeOfFirstCopy);
+        myArrayDeque = resizedArray;
+        firstIndex = 0;
+        lastIndex = size - 1;
+    }
+
+    private void resizeDown() {
+        T[] resizedArray = (T[]) new Object[myArrayDeque.length / 2];
+//        int sizeOfFirstCopy = myArrayDeque.length - firstIndex;
+        if (lastIndex < firstIndex) {
+            int sizeOfFirstCopy = myArrayDeque.length - firstIndex;
+            System.arraycopy(myArrayDeque, firstIndex, resizedArray, 0, sizeOfFirstCopy);
+            System.arraycopy(myArrayDeque, 0, resizedArray, sizeOfFirstCopy,
+                    size - sizeOfFirstCopy);
+
+        } else {
+            System.arraycopy(myArrayDeque, firstIndex, resizedArray, 0, size);
+        }
+        myArrayDeque = resizedArray;
+        firstIndex = 0;
+        lastIndex = size - 1;
+    }
+
+}
---
+++ proj1b/LinkedListDeque.java
@@ -0,0 +1,120 @@
+public class LinkedListDeque<T> {
+    // placeholderType allows us to not immediately define type as int or str.
+    // the first item (if it exists) is at sentinel.next
+    private TypeNode sentinel; // variable
+    private TypeNode lastSentinel; // last sentinel
+    private int size;
+
+    private class TypeNode {
+        private T item;
+        private TypeNode next;
+        private TypeNode prev;
+
+        public TypeNode(T i, TypeNode n, TypeNode p) { //constructor for typenode
+            item = i;
+            next = n;
+            prev = p;
+        }
+    }
+
+    public LinkedListDeque() { // constructor for deque
+        sentinel = new TypeNode(null, null, null);
+        lastSentinel = new TypeNode(null, null, sentinel);
+        sentinel.next = lastSentinel;
+
+        size = 0;
+    }
+
+    public T getRecursive(int index) {
+        TypeNode currentNode = sentinel.next;
+        return getRecursiveHelper(currentNode, index);
+    }
+
+    private T getRecursiveHelper(TypeNode currentNode, int currentIndex) {
+        if (currentIndex < 0 || currentIndex > size || currentNode == null) {
+            return null;
+        } else if (currentIndex == 0) {
+            return currentNode.item;
+        }
+        return getRecursiveHelper(currentNode.next, currentIndex - 1);
+    }
+
+    public void addFirst(T x) {
+        // adds x to the front of the list
+        TypeNode newNode = new TypeNode(x, sentinel.next, sentinel);
+        sentinel.next = newNode;
+        newNode.next.prev = sentinel.next;
+        size += 1;
+    }
+
+    public void addLast(T x) {
+        // add an item to the end of a list
+        size += 1;
+        TypeNode newNode = new TypeNode(x, lastSentinel, lastSentinel.prev);
+        lastSentinel.prev = newNode;
+        newNode.prev.next = newNode;
+    }
+
+    public T removeFirst() {
+        if (size == 0) {
+            return null;
+        }
+        size--;
+        TypeNode removedNode = sentinel.next;
+        removedNode.next.prev = sentinel;
+        sentinel.next = removedNode.next;
+        return removedNode.item;
+    }
+
+    public T removeLast() {
+        if (size == 0) {
+            return null;
+        }
+        size--;
+        TypeNode removedNode = lastSentinel.prev;
+        lastSentinel.prev = removedNode.prev;
+        removedNode.prev.next = lastSentinel;
+        return removedNode.item;
+    }
+
+    public T get(int index) {
+        TypeNode currentNode = sentinel.next;
+        if (index >= size) {
+            return null;
+        }
+        while (index > 0) {
+            currentNode = currentNode.next;
+            index--;
+        }
+        return currentNode.item;
+    }
+
+//    for getRecursive, make helper method that uses the nested class
+
+
+    public int size() {
+        if (size <= 0) {
+            return 0;
+        }
+        return size;
+    }
+
+    public boolean isEmpty() {
+        // returns true if deque is empty, false otherwise
+        return size == 0;
+    }
+
+    public void printDeque() {
+//      start from sentinel print the item by going through the list
+        TypeNode currentNode = sentinel.next;
+        if (currentNode != null) {
+            return;
+        }
+        while (currentNode != lastSentinel) {
+            System.out.print(currentNode.item + " ");
+            currentNode = currentNode.next;
+
+        }
+        System.out.println();
+    }
+}
