23a5acece31e71427815550c37fc249ec512f486
Michelle Sri <michelle.sri@gmail.com>
2018-02-11 19:35:26 -0800

    updated dog and dogLauncher to use java standard library comparator

--- javaNotes/src/java/dog.java
+++ javaNotes/src/java/dog.java
@@ -1,3 +1,5 @@
+import java.util.Comparator;
+
 public class Dog implements Comparable<Dog>{
     private String name;
     private int size;
@@ -11,10 +13,19 @@ public class Dog implements Comparable<Dog>{
         System.out.println(name + " says: bark");
     }
 
+    @Override
     public int compareTo(Dog uddaDog) {
         // returns -1 if this dog is less than the dog pointed at by o...
         return this.size - uddaDog.size;
     }
 
+    private static class NameComparator implements Comparator<Dog> {
+        public int compare(Dog a, Dog b) {
+            return a.name.compareTo(b.name);
+        }
+    }
 
+    public static Comparator<Dog> getNameComparator() {
+        return new NameComparator();
+    }
 }
