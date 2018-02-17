06108d0e86f4e56e44063e769e79a2f3beadcace
Michelle Sri <michelle.sri@gmail.com>
2018-02-17 15:22:57 -0800

    created OffByN.java

---
+++ proj1b/OffByN.java
@@ -0,0 +1,13 @@
+public class OffByN implements CharacterComparator {
+
+    private int n;
+
+    public OffByN (int n) {
+        this.n = n;
+    }
+    @Override
+    public boolean equalChars(char x, char y) {
+        return Math.abs(x - y) == this.n;
+    }
+
+}
