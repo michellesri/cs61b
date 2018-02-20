5647a713820402b6f82d3f43b1c69d50199d444d
Michelle Sri <michelle.sri@gmail.com>
2018-02-20 12:15:39 -0800

    implemented task 1

--- hw1/synthesizer/BoundedQueue.java
+++ hw1/synthesizer/BoundedQueue.java
@@ -7,4 +7,13 @@ public interface BoundedQueue<T> {
     void enqueue(T x);  // add item x to the end
     T dequeue();        // delete and return item from the front
     T peek();           // return (but do not delete) item from the front
+
+    default boolean isEmpty() {
+        // is the buffer empty (fillCount equals zero)?
+        return fillCount() == 0;
+    }
+    default boolean isFull() {
+        // is the buffer full (fillCount is same as capacity)?
+        return fillCount() == capacity();
+    }
 }
