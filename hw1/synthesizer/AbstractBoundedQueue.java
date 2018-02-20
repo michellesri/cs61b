f04ad0f3b6411299c54f943838bc9b23c3b4f3d4
Michelle Sri <michelle.sri@gmail.com>
2018-02-20 15:17:11 -0800

    finished task 4

--- hw1/synthesizer/AbstractBoundedQueue.java
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -1,19 +1,14 @@
 package synthesizer;
 
-public abstract class AbstractBoundedQueue<T> implements BoundedQueue {
+public abstract class AbstractBoundedQueue<T> implements BoundedQueue<T> {
 
     protected int fillCount;
     protected int capacity;
+
     public int capacity() {
-        return fillCount;
-    }
-    public int fillCount() {
         return capacity;
     }
-//    public boolean isEmpty()
-//    public boolean isFull()
-//    public abstract T peek();
-//    public abstract T dequeue();
-//    public abstract void enqueue(T x);
-
+    public int fillCount() {
+        return fillCount;
+    }
 }
--- hw1/synthesizer/ArrayRingBuffer.java
+++ hw1/synthesizer/ArrayRingBuffer.java
@@ -1,10 +1,8 @@
-// TODO: Make sure to make this class a part of the synthesizer package
-// package <package name>;
-import java.util.Iterator;
+package synthesizer;
 
 //TODO: Make sure to make this class and all of its methods public
 //TODO: Make sure to make this class extend AbstractBoundedQueue<t>
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> extends AbstractBoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;            // index for the next dequeue or peek
     /* Index for the next enqueue. */
@@ -15,12 +13,18 @@ public class ArrayRingBuffer<T>  {
     /**
      * Create a new ArrayRingBuffer with the given capacity.
      */
+
     public ArrayRingBuffer(int capacity) {
         // TODO: Create new array with capacity elements.
         //       first, last, and fillCount should all be set to 0.
         //       this.capacity should be set appropriately. Note that the local variable
         //       here shadows the field we inherit from AbstractBoundedQueue, so
         //       you'll need to use this.capacity to set the capacity.
+        this.capacity = capacity;
+        rb = (T[]) new Object[this.capacity];
+        first = 0;
+        last = 0;
+        fillCount = 0;
     }
 
     /**
@@ -30,6 +34,14 @@ public class ArrayRingBuffer<T>  {
      */
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update last.
+        if (fillCount == capacity) {
+            throw new RuntimeException("Ring buffer overflow");
+        }
+        rb[last] = x;
+        fillCount++;
+
+        last = (last + 1) % capacity;
+
     }
 
     /**
@@ -38,7 +50,16 @@ public class ArrayRingBuffer<T>  {
      * covered Monday.
      */
     public T dequeue() {
-        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update 
+        // TODO: Dequeue the first item. Don't forget to decrease fillCount and update first
+        if (fillCount() == 0) {
+            throw new RuntimeException("Ring buffer underflow");
+        } else {
+            T temp = rb[first];
+            rb[first] = null;
+            fillCount--;
+            first = (first + 1) % capacity;
+            return temp;
+        }
     }
 
     /**
@@ -46,6 +67,7 @@ public class ArrayRingBuffer<T>  {
      */
     public T peek() {
         // TODO: Return the first item. None of your instance variables should change.
+        return rb[first];
     }
 
     // TODO: When you get to part 5, implement the needed code to support iteration.
