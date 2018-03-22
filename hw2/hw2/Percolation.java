1e370d14d7e1d4ac43eb1f0bda69d53a2cf42618
Michelle Sri <michelle.sri@gmail.com>
2018-03-22 16:31:14 -0700

    finished percolation and percolationStats

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -9,10 +9,12 @@ public class Percolation {
     private int N;
     private WeightedQuickUnionUF WQU;
 
+    private final int topSentinel = N * N + 1;
+    private final int bottomSentinel = N * N + 2;
 
     public Percolation(int N) {
 
-        WQU = new WeightedQuickUnionUF(N * N);
+        WQU = new WeightedQuickUnionUF(N * N + 2);
         // create N-by-N grid, with all sites initially blocked
         openSites = 0;
         this.N = N;
@@ -24,15 +26,65 @@ public class Percolation {
         }
     }
 
+    public void connectToTop(int currentCell) {
+        WQU.union(topSentinel, currentCell);
+    }
+
+    public void connectToBottom(int currentCell) {
+        WQU.union(bottomSentinel, currentCell);
+    }
+
     public void open(int row, int col) {
     // open the site (row, col) if it is not open already
         if (!grid[row][col]) {
             grid[row][col] = true;
             openSites++;
+            if (row == 0) {
+                connectToTop(xyTo1D(row, col));
+            } else if (row == N - 1) {
+                connectToBottom(xyTo1D(row, col));
 
-            // TODO: check all neighbors. if there are neighbors call union on open neighbors
-            // WQU.union(convert into ints, other int)
+            }
+
+            // check all neighbors. if there are neighbors call union on open neighbors
+            mergeOpenNeighbors(row, col);
         }
+    }
+
+    public void mergeOpenNeighbors(int row, int col) {
+        int topNeighborRow = row - 1;
+        int bottomNeighborRow = row + 1;
+
+        int leftNeighborCol = col - 1;
+        int rightNeighborCol = col + 1;
+
+        boolean topNeighborOpen = isOpen(topNeighborRow, col);
+        boolean bottomNeighborOpen = isOpen(bottomNeighborRow, col);
+
+        boolean leftNeighborOpen = isOpen(row, leftNeighborCol);
+        boolean rightNeighborOpen = isOpen(row, rightNeighborCol);
+
+        int centerCell = xyTo1D(row, col);
+
+        if (topNeighborOpen) {
+            WQU.union(xyTo1D(topNeighborRow, col), centerCell);
+        }
+
+        if (bottomNeighborOpen) {
+            WQU.union(xyTo1D(bottomNeighborRow, col), centerCell);
+        }
+
+        if (rightNeighborOpen) {
+            WQU.union(xyTo1D(row, rightNeighborCol), centerCell);
+        }
+
+        if (leftNeighborOpen) {
+            WQU.union(xyTo1D(row, leftNeighborCol), centerCell);
+        }
+    }
+
+    public boolean isValidCoordinate(int row, int col) {
+        return row < N && row >= 0 && col < N && col >= 0;
     }
     public boolean isOpen(int row, int col) {
     // is the site (row, col) open?
@@ -42,6 +94,9 @@ public class Percolation {
     public boolean isFull(int row, int col) {
     // is the site (row, col) full?
         // check if the cell is full by seeing if any in the set of WQU are connected to the top
+        int currentCell = xyTo1D(row, col);
+        return WQU.connected(currentCell, topSentinel);
+
 
     }
     public int numberOfOpenSites() {
@@ -51,6 +106,7 @@ public class Percolation {
     public boolean percolates() {
     // does the system percolate?
         // check if one set in the WQU has a cell that's connected to the top and bottom
+        return WQU.connected(topSentinel, bottomSentinel);
     }
 
     public int xyTo1D(int r, int c) {
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,76 @@
 package hw2;
 
+import edu.princeton.cs.algs4.StdRandom;
+
 public class PercolationStats {
 
+    int[] openSitesPerT;
+    // array of number of open sites in each test
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+
+        openSitesPerT = new int[T];
+        // perform T independent experiments on an N-by-N grid
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+
+        while (T > 0) {
+            Percolation percolation = pf.make(N);
+            int openSites = 0;
+
+            while (!percolation.percolates()) {
+
+                StdRandom.setSeed(System.currentTimeMillis());
+
+                int randomRow = StdRandom.uniform(N);
+                int randomCol = StdRandom.uniform(N);
+
+                percolation.open(randomRow, randomCol);
+                openSites++;
+            }
+            openSitesPerT[T] = openSites;
+            T--;
+        }
+
+    }
+
+    public double mean() {
+        // sample mean of percolation threshold
+        int count = 0;
+        for (int i : openSitesPerT) {
+            count += i;
+        }
+
+        return count / openSitesPerT.length;
+    }
+
+    public double stddev() {
+        // sample standard deviation of percolation threshold
+        double mean = mean();
+
+        int total = 0;
+
+        for (int i : openSitesPerT) {
+            total += Math.pow((i - mean), 2);
+        }
+
+        return total / openSitesPerT.length - 1;
+    }
+
+    public double confidenceLow() {
+        // low endpoint of 95% confidence interval
+        double mean = mean();
+        double stddev = stddev();
+
+        return mean - 1.96 * stddev / Math.sqrt(openSitesPerT.length);
+    }
+
+    public double confidenceHigh() {
+        // high endpoint of 95% confidence interval
+        double mean = mean();
+        double stddev = stddev();
+
+        return mean + 1.96 * stddev / Math.sqrt(openSitesPerT.length);
+    }
 }
