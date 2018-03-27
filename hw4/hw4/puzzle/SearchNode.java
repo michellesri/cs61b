74eeaeae0fa334b42f3a1e66751605283b5ab2e3
Michelle Sri <michelle.sri@gmail.com>
2018-03-26 20:52:46 -0700

    working on solver

---
+++ hw4/hw4/puzzle/SearchNode.java
@@ -0,0 +1,33 @@
+package hw4.puzzle;
+
+import java.util.Comparator;
+
+public class SearchNode {
+    WorldState worldState;
+    int numMovesToThisState;
+    SearchNode prevSearchNode;
+
+    public SearchNode(WorldState worldState, int numMovesToThisState, SearchNode prevSearchNode) {
+        this.worldState = worldState;
+        this.numMovesToThisState = numMovesToThisState;
+        this.prevSearchNode = prevSearchNode;
+    }
+
+    public static class SearchNodeComparator implements Comparator<SearchNode> {
+
+        @Override
+        public int compare(SearchNode o1, SearchNode o2) {
+            int o1Priority = o1.worldState.estimatedDistanceToGoal() + o1.numMovesToThisState;
+            int o2Priority = o2.worldState.estimatedDistanceToGoal() + o2.numMovesToThisState;
+
+            if (o1Priority < o2Priority) {
+                return -1;
+            } else if (o1Priority == o1Priority){
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    }
+
+}
---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,68 @@
+package hw4.puzzle;
+
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.*;
+
+public class Solver {
+
+    private Set<WorldState> processedWorldStates = new HashSet<>();
+    private MinPQ<SearchNode> minPQ = new MinPQ<>(1, new SearchNode.SearchNodeComparator());
+    private SearchNode winningNode;
+
+
+
+    public Solver(WorldState initial) {
+        // Constructor which solves the puzzle, computing
+        // everything necessary for moves() and solution() to
+        // not have to solve the problem again. Solves the
+        // puzzle using the A* algorithm. Assumes a solution exists.
+
+        SearchNode initialNode = new SearchNode(initial, 0, null);
+        minPQ.insert(initialNode);
+
+        SearchNode x = minPQ.delMin();
+        processedWorldStates.add(x.worldState);
+
+        while (!x.worldState.isGoal()) {
+            for (WorldState neighborWorldState : x.worldState.neighbors()) {
+                if (processedWorldStates.contains(neighborWorldState)) {
+                    continue;
+                }
+                minPQ.insert(new SearchNode(neighborWorldState, x.numMovesToThisState + 1, x));
+            }
+            x = minPQ.delMin();
+            processedWorldStates.add(x.worldState);
+        }
+
+        winningNode = x;
+    }
+
+    public int moves() {
+        // Returns the minimum number of moves to solve the puzzle starting
+        // at the initial WorldState.
+        return winningNode.numMovesToThisState;
+
+    }
+
+    public Iterable<WorldState> solution() {
+        // Returns a sequence of WorldStates from the initial WorldState
+        // to the solution.
+
+        List<WorldState> pathToVictoryReverse = new ArrayList<>();
+        List<WorldState> pathToVictory = new ArrayList<>();
+
+        SearchNode currentNode = winningNode;
+
+        while (currentNode.prevSearchNode != null) {
+            pathToVictoryReverse.add(currentNode.worldState);
+            currentNode = currentNode.prevSearchNode;
+        }
+
+        for (int i = pathToVictoryReverse.size() - 1; i > 0 ; i--) {
+            pathToVictory.add(pathToVictoryReverse.get(i));
+        }
+
+        return pathToVictory;
+    }
+}
