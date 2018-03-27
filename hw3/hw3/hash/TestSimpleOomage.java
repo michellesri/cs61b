c522f854936de4a073f123d865c8aef94ac607b3
Michelle Sri <michelle.sri@gmail.com>
2018-03-26 20:59:07 -0700

    changed import to be more specific

--- hw3/hw3/hash/TestSimpleOomage.java
+++ hw3/hw3/hash/TestSimpleOomage.java
@@ -1,13 +1,15 @@
 package hw3.hash;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
-
-
-import java.util.*;
-
 
 public class TestSimpleOomage {
 
