ac3ed011bd9dba312cbdc1e26f54ecb660def7a5
Michelle Sri <michelle.sri@gmail.com>
2018-04-13 22:15:47 -0700

    finished quicksort class

--- lab12/QuickSort.java
+++ lab12/QuickSort.java
@@ -49,14 +49,39 @@ public class QuickSort {
     private static <Item extends Comparable> void partition(
             Queue<Item> unsorted, Item pivot,
             Queue<Item> less, Queue<Item> equal, Queue<Item> greater) {
-        // Your code here!
+
+        for (Item item : unsorted) {
+            if (item.compareTo(pivot) == -1) {
+                less.enqueue(item);
+            } else if (item.compareTo(pivot) == 0) {
+                equal.enqueue(item);
+            } else {
+                greater.enqueue(item);
+            }
+        }
     }
 
     /** Returns a Queue that contains the given items sorted from least to greatest. */
     public static <Item extends Comparable> Queue<Item> quickSort(
             Queue<Item> items) {
-        // Your code here!
-        return items;
+
+        if (items.size() == 1 || items.size() < 1) {
+            return items;
+        }
+
+        Item pivot = getRandomItem(items);
+
+        Queue<Item> less = new Queue<>();
+        Queue<Item> equal = new Queue<>();
+        Queue<Item> greater = new Queue<>();
+
+        partition(items, pivot, less, equal, greater);
+
+        Queue<Item> lessQueue = quickSort(less);
+        Queue<Item> greaterQueue = quickSort(greater);
+
+        return catenate(catenate(lessQueue, equal), greaterQueue);
+
     }
 
     public static void main(String[] args) {
