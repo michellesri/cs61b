5922f16b0f98e8b27d556582c4a9561253133a83
Michelle Sri <michelle.sri@gmail.com>
2018-04-22 20:52:02 -0700

    finished radixSort

--- lab13/RadixSort.java
+++ lab13/RadixSort.java
@@ -18,49 +18,78 @@ public class RadixSort {
      * @return String[] the sorted array
      */
     public static String[] sort(String[] asciis) {
-        // TODO: Implement LSD Sort
+        // find the longest ascii. pad all the other characters with an imaginary character
+            // call sortHelper
+        char blankspace = '_';
 
-        return null;
+        String[] asciisCopyPadded = new String[asciis.length];
+        String[] asciisCopy = new String[asciis.length];
+
+        int longestLength = 0;
+        for (int i = 0; i < asciis.length; i++) {
+            int currentLength = asciis[i].length();
+            if (currentLength > longestLength) {
+                longestLength = currentLength;
+            }
+            asciisCopyPadded[i] = asciis[i];
+            asciisCopy[i] = asciis[i];
+        }
+
+        for (int i = 0; i < asciisCopyPadded.length; i++) {
+            String currentAscii = asciisCopyPadded[i];
+            while (currentAscii.length() < longestLength) {
+                currentAscii += blankspace;
+            }
+        }
+
+        for (int i = asciisCopyPadded.length - 1; i > 0; i--) {
+            sortHelperLSD(asciisCopyPadded, asciisCopy, i);
+        }
+        return asciisCopy;
     }
 
     /**
      * LSD helper method that performs a destructive counting sort the array of
      * Strings based off characters at a specific index.
-     * @param asciis Input array of Strings
+     * @param asciisCopyPadded Input array of Strings
      * @param index The position to sort the Strings on.
      */
-    private static void sortHelperLSD(String[] asciis, int index) {
+    private static void sortHelperLSD(String[] asciisCopyPadded, String[] asciisCopy, int index) {
         // Optional LSD helper method for required LSD radix sort
-        int[] intValsArray = new int[asciis.length];
+        int[] intValsArray = new int[asciisCopyPadded.length];
 
-        for (int i = 0; i < asciis.length; i++) {
-            int charAtCurrentIndex = asciis[i].charAt(index);
+        for (int i = 0; i < asciisCopyPadded.length; i++) {
+            int charAtCurrentIndex = asciisCopyPadded[i].charAt(index);
             intValsArray[i] = charAtCurrentIndex;
         }
 
         for (int i = 0; i < intValsArray.length; i++) {
-        int min = Integer.MAX_VALUE;
-        int minIndex = 0;
+            int min = Integer.MAX_VALUE;
+            int minIndex = 0;
             for (int j = i; j < intValsArray.length; j++) {
                 if (intValsArray[j] < min) {
                     min = intValsArray[j];
                     minIndex = j;
                 }
             }
-            swap(i, minIndex, intValsArray, asciis);
+            swap(i, minIndex, intValsArray, asciisCopyPadded, asciisCopy);
         }
 
         return;
     }
 
-    private static void swap(int i, int j, int[] intValsArray, String[] asciis) {
+    private static void swap(int i, int j, int[] intValsArray, String[] asciisCopyPadded, String[] asciisCopy) {
         int temp = intValsArray[i];
         intValsArray[i] = intValsArray[j];
         intValsArray[j] = temp;
 
-        String temp2 = asciis[i];
-        asciis[i] = asciis[j];
-        asciis[j] = temp2;
+        String tempCopyPadded = asciisCopyPadded[i];
+        asciisCopyPadded[i] = asciisCopyPadded[j];
+        asciisCopyPadded[j] = tempCopyPadded;
+
+        String tempCopy = asciisCopy[i];
+        asciisCopy[i] = asciisCopy[j];
+        asciisCopy[j] = tempCopy;
     }
 
     public static void main(String[] args) {
@@ -69,11 +98,10 @@ public class RadixSort {
         blah[1] = "aaa";
         blah[2] = "ccc";
 
-        sortHelperLSD(blah, 2);
+//        sortHelperLSD(blah, 2);
         System.out.println(Arrays.toString(blah));
     }
 
-    //there is a bug in my helper function
     // in the sort function, call it on every index from LSD until index 0
 
     /**
